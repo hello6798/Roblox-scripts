d1f68a0b774cbeed2648bafc2b0807977a9883ec
hello6798 <105911396+hello6798@users.noreply.github.com>
2022-07-06 21:15:38 -0600

    Fixed

--- death brick.lua
+++ death brick.lua
@@ -1,6 +1,6 @@
 local part = Instance.new("Part")
 part.Name="DEATHPART"
-part.Position=player2.HumanoidRootPart.CFrame
+part.Position=Vector3.new(0, 0, 0) --set position here
 part.Parent=game.Workspace
 game.Workspace.DEATHPART:Touched:Connect(Function(hit)
 if game.Players:GetPlayerFromCharacter(hit.Parent) then
