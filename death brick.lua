23762ab7a63dee8991083999ae8a2ad815665157
hello6798 <105911396+hello6798@users.noreply.github.com>
2022-07-06 20:18:25 -0600

    Update death brick.lua

--- death brick.lua
+++ death brick.lua
@@ -1,11 +1,9 @@
-player=game.Players.LocalPlayer.Character.Name
-player2=Workspace[player]
 local part = Instance.new("Part")
 part.Name="DEATHPART"
 part.Position=player2.HumanoidRootPart.CFrame
 part.Parent=game.Workspace
 game.Workspace.DEATHPART:Touched:Connect(Function(hit)
 if game.Players:GetPlayerFromCharacter(hit.Parent) then
-player2[hit.Name]:Destroy
+hit:destroy
 end
 end)
