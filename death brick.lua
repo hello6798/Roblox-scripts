60078ff6b16a0b1d5d9dbaafa5e5e0005bec38dd
hello6798 <105911396+hello6798@users.noreply.github.com>
2022-07-06 22:07:23 -0600

    Fixed

--- death brick.lua
+++ death brick.lua
@@ -2,8 +2,6 @@ local part = Instance.new("Part")
 part.Name="DEATHPART"
 part.Position=Vector3.new(0, 0, 0) --set position here
 part.Parent=game.Workspace
-game.Workspace.DEATHPART:Touched:Connect(Function(hit)
-if game.Players:GetPlayerFromCharacter(hit.Parent) then
-hit:destroy
-end
+game.Workspace.DEATHPART.Touched:Connect(function(hit)
+hit:Destroy()
 end)
